94cae75fa8b96cfa6a17ec9344dc7741956f2bfd
brintal <brintal@gmail.com>
2019-12-13 00:23:10 +0100

    restructure angular project

--- buildComplete.bat
+++ buildComplete.bat
@@ -1,3 +1,4 @@
+CALL mvn build-helper:parse-version versions:set -DnewVersion=${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.nextIncrementalVersion}-SNAPSHOT versions:commit
 CALL mvn clean install -Dmaven.test.skip=true
 cd app
 CALL mvn dockerfile:push
